96196f0df7c714a8b2740d5d971eb45c39a61a87
lapexp <lapexp@outlook.com>
2022-06-16 19:55:08 -0700

    Initial load

--- js/book.js
+++ js/book.js
@@ -5,6 +5,18 @@ function createBookData() {
   let isbn = document.getElementById("isbn").value;
   let subject = document.getElementById("subject").value;
   let numPages = document.getElementById("numPages").value;
+  let allAvailabilityValues = document.getElementsByName("availability");
+
+  //in case of checkbox we have to take array
+  //let availability =[];
+  let availability;
+  allAvailabilityValues.forEach((node) => {
+    if (node.checked) {
+      //in case of checkbox we have to push the value
+      availability = node.value;
+    }
+  });
+
   let obj = {
     title: title,
     writer: writer,
@@ -12,6 +24,46 @@ function createBookData() {
     isbn: isbn,
     subject: subject,
     numPages: numPages,
+    availability: availability,
   };
-  console.log(obj);
+
+  fetch("http://localhost:3000/books", {
+    method: "POST",
+    body: JSON.stringify(obj),
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+  }).then((res) => res.json());
+}
+
+function getBooksData() {
+  fetch("http://localhost:3000/books")
+    .then((res) => res.json())
+    .then((data) => {
+      let tBody = document.getElementById("tBody");
+      let templates = "";
+      for (let i = 0; i < data.length; i++) {
+        templates = templates + getRowTemplate(data[i]);
+      }
+      tBody.innerHTML = templates;
+      //console.log(data);
+    });
+}
+
+function getRowTemplate(obj) {
+  let template = `
+  <tr>
+    <td>${obj.title}</td>
+    <td>${obj.writer}</td>
+    <td>${obj.publisher}</td>
+    <td>${obj.isbn}</td>
+    <td>${obj.subject}</td>
+    <td>${obj.availability}</td>
+    <td>${obj.numPages}</td>
+  </tr>`;
+  return template;
+}
+function goToUrl(relativeUrl) {
+  location.herf = relativeUrl;
 }
